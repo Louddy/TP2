4fe5dab092a91d5707eeabeefd38d9e5aa0d03ea
1651693 <1651693@li-h015-0t18>
2017-11-14 12:10:19 -0500

    sout(f)

---
+++ TP2/src/ca/qc/bdeb/info203/TP2/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.qc.bdeb.info203.TP2;
+
+/**
+ *
+ * @author 1651693
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("f");
+    }
+    
+}
